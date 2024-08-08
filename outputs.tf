7bb66e58b0219f80ad9be3ce660f86d3204fef25
Stephen Hoekstra <shoekstra@schubergphilis.com>
2024-08-08 17:30:53 +0200

    fix: Add missing outputs
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,27 @@ output "arn" {
   value       = aws_acm_certificate.default.arn
 }
 
+output "domain_name" {
+  description = "Domain name for which the certificate is issued"
+  value       = aws_acm_certificate.default.domain_name
+}
+
 output "id" {
   description = "Certificate ID"
   value       = aws_acm_certificate.default.id
+}
+
+output "not_after" {
+  description = "Expiration date and time of the certificate"
+  value       = aws_acm_certificate.default.not_after
+}
+
+output "not_before" {
+  description = "Start of the validity period of the certificate"
+  value       = aws_acm_certificate.default.not_before
+}
+
+output "status" {
+  description = "Status of the certificate"
+  value       = aws_acm_certificate.default.status
 }
